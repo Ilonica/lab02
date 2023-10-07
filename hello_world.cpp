20d1b85052c4626d3a3e26bef0b475cba2b4f54f
Ilonica <i.venediktova@inbox.ru>
2023-10-07 15:43:27 +0800

    delete using namespace std

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <string>
-using namespase std;
 int main(){
-string  name;
-cin >> name;
-cout << "Hello world from" << name;
+string name;
+std::cin >> name;
+std::cout << "Hello world from" << name;
 }
